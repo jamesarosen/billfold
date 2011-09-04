9e3ca6f8fe123011624846d15470508b7ab5057c
James A. Rosen <james@zendesk.com>
2011-09-04 16:57:08 -0700

    AuthenticationController goes in the Billfold module

--- app/controllers/billfold/authentication_controller.rb
+++ app/controllers/billfold/authentication_controller.rb
@@ -1,21 +1,23 @@
-class AuthenticationController < ApplicationController
+module Billfold
+  class AuthenticationController < ApplicationController
 
-  respond_to :html, :json, :xml
+    respond_to :html, :json, :xml
 
-  def update_or_create
-    omniauth_hash = request.env['omniauth.auth'] || {}
-    identity = Billfold.identity_class.update_or_create!({
-      :provider => params[:provider],
-      :value    => omniauth_hash['uid'],
-      :data     => omniauth_hash['user_info'],
-      :user     => current_user
-    })
-    self.current_user ||= identity.user
-    respond_to do |format|
-      format.html { redirect_to '/' }
-      format.json { head 201 }
-      format.xml  { head 201 }
+    def update_or_create
+      omniauth_hash = request.env['omniauth.auth'] || {}
+      identity = Billfold.identity_class.update_or_create!({
+        :provider => params[:provider],
+        :value    => omniauth_hash['uid'],
+        :data     => omniauth_hash['user_info'],
+        :user     => current_user
+      })
+      self.current_user ||= identity.user
+      respond_to do |format|
+        format.html { redirect_to '/' }
+        format.json { head 201 }
+        format.xml  { head 201 }
+      end
     end
-  end
 
+  end
 end
