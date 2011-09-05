f5d0598f644002c77cf1a2ce8498e944dbb91d8d
James A. Rosen <james@zendesk.com>
2011-09-05 15:57:27 -0700

    /auth/:provider/callback routes for all verbs

--- spec/controllers/identities_controller_spec.rb
+++ spec/controllers/identities_controller_spec.rb
@@ -2,7 +2,16 @@ require 'spec_helper'
 
 describe Billfold::IdentitiesController do
 
-  it 'routes /auth/:provider/callback to update_or_create' do
+  it 'routes GET /auth/:provider/callback to update_or_create' do
+    { :get => '/auth/cheez/callback' }.
+      should route_to(
+        :controller => 'billfold/identities',
+        :action => 'update_or_create',
+        :provider => 'cheez'
+      )
+  end
+
+  it 'routes POST /auth/:provider/callback to update_or_create' do
     { :post => '/auth/cheez/callback' }.
       should route_to(
         :controller => 'billfold/identities',
