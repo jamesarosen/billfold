40ea4b34901d566f556cc74d39d94e51afe4f971
James A. Rosen <james@zendesk.com>
2011-09-04 17:23:43 -0700

    Made ControllerSupport a concern; #current_user is available as a helper method

--- lib/billfold/controller_support.rb
+++ lib/billfold/controller_support.rb
@@ -1,3 +1,4 @@
+require 'active_support/concern'
 require 'billfold'
 
 module Billfold
@@ -7,32 +8,42 @@ module Billfold
   # Gets mixed in to `ApplicationController` automatically.
   module ControllerSupport
 
-    protected
+    extend ActiveSupport::Concern
 
-    # ### Billfold::ControllerSupport#current_user
-    #
-    # Return the current user, if signed in.
-    def current_user
-      @current_user ||= ::Billfold::user_class.find_by_id(session[:user_id])
+    included do
+      helper_method :current_user
     end
 
-    # ### Billfold::ControllerSupport#current_user=
-    #
-    # Set the signed-in user.
-    def current_user=(user)
-      session[:user_id] = user ? user.id : nil
-      @current_user = user
-    end
+    module InstanceMethods
 
-    # ### Billfold::ControllerSupport#require_sign_in
-    #
-    # A before filter that requires a user to be signed in. The default
-    # implementation adds a flash message and redirects to /
-    def require_sign_in
-      unless current_user
-        flash['info'] = 'Please sign in'
-        redirect_to root_path
+      protected
+
+      # ### Billfold::ControllerSupport#current_user
+      #
+      # Return the current user, if signed in.
+      def current_user
+        @current_user ||= ::Billfold::user_class.find_by_id(session[:user_id])
       end
+
+      # ### Billfold::ControllerSupport#current_user=
+      #
+      # Set the signed-in user.
+      def current_user=(user)
+        session[:user_id] = user ? user.id : nil
+        @current_user = user
+      end
+
+      # ### Billfold::ControllerSupport#require_sign_in
+      #
+      # A before filter that requires a user to be signed in. The default
+      # implementation adds a flash message and redirects to /
+      def require_sign_in
+        unless current_user
+          flash['info'] = 'Please sign in'
+          redirect_to root_path
+        end
+      end
+
     end
 
   end
